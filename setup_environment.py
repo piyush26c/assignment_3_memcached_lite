e30ca9d30acd6fa7c3b93b047f760349e954c077
Piyush Chaudhari <piyrchau@iu.edu>
2023-10-06 15:36:50 -0400

    pip install code added

--- setup_environment.py
+++ setup_environment.py
@@ -10,8 +10,24 @@ if current_version < required_version:
     print(f"Python version {current_version} is not compatible. Installing Python {required_version}...")
     subprocess.run(["sudo", "apt-get", "install", f"python{required_version}"])
 
-# Run the 'apt' command to install pip for Python 3
-subprocess.run(["sudo", "apt", "install", "python3-pip"], check=True)
+import os
+import subprocess
+import sys
+
+# Check if pip is already installed
+try:
+    import pip
+    print("pip is already installed.")
+except ImportError:
+    # Install pip
+    print("pip is not installed. Installing pip...")
+    try:
+        subprocess.check_call([sys.executable, '-m', 'ensurepip', '--default-pip'])
+        print("pip has been successfully installed.")
+    except subprocess.CalledProcessError as e:
+        print(f"Failed to install pip: {e}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 # Install requirements from requirements.txt
 subprocess.run(["pip", "install", "-r", "requirements.txt", "-v"])
