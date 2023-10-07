bcaa067bd8a87b5854ae471207b0bdd645e4f408
Piyush Chaudhari <piyrchau@iu.edu>
2023-10-06 23:15:42 -0400

    performance testing code added and gitignore added

---
+++ performance_testing/client_get_read_latency.py
@@ -0,0 +1,36 @@
+import socket
+import sys
+import time
+
+if __name__ == '__main__':
+    """
+    Performance Test case - 01:
+    Clocking in the time required for get and set using Google Cloud Storage service for files (having 1, 10, 100, 1000, 10000, 100000 number of key-value pairs)
+    Also, the key and value sizes are in range(min_key/value_size, max_key/value_size)
+    
+    THIS CLIENT CODE CLOCKS GET (READ) TIMINGS.
+    """
+    if len(sys.argv) == 3:
+        host_name = str(sys.argv[2])
+        port = int(sys.argv[1])
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_socket.connect((host_name, port))
+        time1 = time2 = 0
+        while True:
+            command = input('\nClient Input: ')
+            if command.lower().strip() == "exit":
+                break
+
+            time1 = time.time()
+            client_socket.send(command.encode('utf-8'))
+            if command.split()[0] == "set":
+                command = input('Client Input: ')
+                client_socket.send(command.encode('utf-8'))
+            
+            response = client_socket.recv(9000).decode('utf-8')
+            time2 = time.time()
+            print("Server Response:\n" + response)
+            print("READ LATENCY (GET command):", time2 - time1)
+        client_socket.close()
+    else:
+        print("Inappropriate arguments passed. (eg. python3 client.py <port_number> <server_ip_address>)")
--- performance_testing/data_generator.py
+++ performance_testing/data_generator.py
@@ -1,10 +1,46 @@
 import sys
+import json
+import string
+import random
 
+def generate_random_string(size):
+    # Define the characters you want to use
+    characters = string.ascii_letters  # Contains both uppercase and lowercase letters
+
+    # Generate the random string by selecting characters randomly
+    random_string = ''.join(random.choice(characters) for _ in range(size))
+
+    return random_string
 
 
 if __name__ == '__main__':
-    if len(sys.argv) == 3:
+    if len(sys.argv) == 6:
+        number_of_kv_pairs = int(sys.argv[1])
+        min_key_size = int(sys.argv[2])
+        max_key_size = int(sys.argv[3])
+        min_val_size = int(sys.argv[4])
+        max_val_size = int(sys.argv[5])
         
+        file_name = "key_value_" + str(number_of_kv_pairs) + ".json"
+        with open(file_name, 'w') as f:
+                json.dump({}, f)
+
+        data = None
+        # read json file
+        with open(file_name, 'r') as file:
+                data = json.load(file)
+        
+        for indx in range(number_of_kv_pairs):
+            key = generate_random_string(random.randint(min_key_size, max_key_size))
+            value = generate_random_string(random.randint(min_val_size, max_val_size))
+
+            data[key] = value
+        
+        # save data to json file and write it on disk
+        with open(file_name, 'w') as file:
+            json.dump(data, file, indent=4)
+    else:
+        print("Inappropriate number of arguments. (python3 data_generator.py <number_of_key_value_pairs> <minimum_key_size> <maximum_key_size> <minimum_value_size> <maximum_value_size>")
 
 
 
